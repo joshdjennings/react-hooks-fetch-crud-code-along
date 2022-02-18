d0b5d22e7f8a834aa36f67056c316a4377bb9bc6
joshua david jennings <jennings.josh@gmail.com>
2022-02-18 16:46:16 -0600

    Walkthrough done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,36 +1,67 @@
-import React, { useState } from "react";
-import ItemForm from "./ItemForm";
-import Filter from "./Filter";
-import Item from "./Item";
+import React, { useEffect, useState } from 'react';
+import ItemForm from './ItemForm';
+import Filter from './Filter';
+import Item from './Item';
 
 function ShoppingList() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [items, setItems] = useState([]);
+	const [selectedCategory, setSelectedCategory] = useState('All');
+	const [items, setItems] = useState([]);
 
-  function handleCategoryChange(category) {
-    setSelectedCategory(category);
-  }
+	useEffect(() => {
+		fetch('http://localhost:4000/items')
+			.then((r) => r.json())
+			.then((items) => setItems(items));
+	}, []);
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+	function handleDeleteItem(deletedItem) {
+		const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+		setItems(updatedItems);
+	}
 
-    return item.category === selectedCategory;
-  });
+	function handleUpdateItem(updatedItem) {
+		const updatedItems = items.map((item) => {
+			if (item.id === updatedItem.id) {
+				return updatedItem;
+			} else {
+				return item;
+			}
+		});
+		setItems(updatedItems);
+	}
 
-  return (
-    <div className="ShoppingList">
-      <ItemForm />
-      <Filter
-        category={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
-      </ul>
-    </div>
-  );
+	function handleAddItem(newItem) {
+		setItems([...items, newItem]);
+	}
+
+	function handleCategoryChange(category) {
+		setSelectedCategory(category);
+	}
+
+	const itemsToDisplay = items.filter((item) => {
+		if (selectedCategory === 'All') return true;
+
+		return item.category === selectedCategory;
+	});
+
+	return (
+		<div className="ShoppingList">
+			<ItemForm onAddItem={handleAddItem} />
+			<Filter
+				category={selectedCategory}
+				onCategoryChange={handleCategoryChange}
+			/>
+			<ul className="Items">
+				{itemsToDisplay.map((item) => (
+					<Item
+						key={item.id}
+						item={item}
+						onUpdateItem={handleUpdateItem}
+						onDeleteItem={handleDeleteItem}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default ShoppingList;
